b8edb17437dabcc108c6e5c837510706711b4d47
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2025-09-17 21:17:40 -0400

    game loop and getting player position input features

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,35 @@
 const std = @import("std");
 
-const State = struct { turn: u8, board: [9]u8 };
+const State = struct { turn: u8, board: [9]u8, game_over: bool };
 pub fn main() !void {
-    const state = State{
-        .turn = 1,
-        .board = .{ '_', '_', '_', '_', '_', '_', '_', '_', '_' },
-    };
+    var state = State{ .turn = 1, .board = .{ '_', '_', '_', '_', '_', '_', '_', '_', '_' }, .game_over = false };
 
-    try print_board(state);
+    var in_buffer: [1024]u8 = undefined;
+    var stdin = std.fs.File.stdin().reader(&in_buffer);
+    var stdin_interface = &stdin.interface;
+
+    while (!state.game_over) {
+        try print_board(state);
+
+        const input = try stdin_interface.takeDelimiterExclusive('\n');
+
+        if (std.mem.eql(u8, input, "q")) {
+            state.game_over = true;
+            break;
+        }
+
+        const pos = std.fmt.parseInt(u8, input, 10) catch {
+            std.debug.print("Failed to parse input, please you a number between 0 and 8", .{});
+            continue;
+        };
+
+        player_input(&state, pos);
+
+        state.turn += 1;
+    }
 }
 
-pub fn print_board(state: State) !void {
+fn print_board(state: State) !void {
     var out_buffer: [1024]u8 = undefined;
 
     var stdout_writer = std.fs.File.stdout().writer(&out_buffer);
@@ -22,4 +41,20 @@ pub fn print_board(state: State) !void {
     try stdout_interface.print("| {c} | {c} | {c} | \n", .{ state.board[6], state.board[7], state.board[8] });
 
     try stdout_interface.flush();
+}
+
+fn player_input(state: *State, position: u8) void {
+    const player = state.turn % 2;
+
+    switch (player) {
+        1 => {
+            state.board[position] = 'X';
+        },
+        0 => {
+            state.board[position] = 'O';
+        },
+        else => {
+            std.debug.print("Something went wrong with adding player symbol to board position!", .{});
+        },
+    }
 }
